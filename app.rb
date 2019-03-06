545f012fa50203f93ca96b9ba85226381ae9478d
kudari00 <yehong@brandeis.edu>
2019-03-06 18:14:40 -0500

    CORS

--- app.rb
+++ app.rb
@@ -12,6 +12,24 @@ class App < Sinatra::Base
   # set :port, 8000
   # set :bind, '127.0.0.1'
 
+  configure do
+    enable :cross_origin
+    enable :sessions
+    set :sessions, :expire_after => 2592000
+    set :session_store, Rack::Session::Pool
+  end
+  before do
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
+  
+  # routes...
+  options "*" do
+    response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    200
+  end
+
   get '/' do
     redirect 'index.html'
   end
@@ -21,6 +39,7 @@ class App < Sinatra::Base
   post '/api/user/signup' do
     user = User.new(params)
     if user.save
+      status 201
       {message: "Signup success!"}.to_json
     else
       status 403
@@ -32,10 +51,14 @@ class App < Sinatra::Base
   # sign in
   post '/api/user/signin' do
     if User.authenticate(params[:email], params[:password])
-      User.find_by_email(params[:email]).to_json
+      user = User.find_by_email(params[:email])
     else
-      error 404, {error: "Username and password do not match!"}.to_json
+      error 403, {error: "Username and password do not match!"}.to_json
     end
+  end
+
+  delete '/api/user/signout' do 
+    session[:user] = nil;
   end
 
 end
