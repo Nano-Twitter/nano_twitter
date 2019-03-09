c22dca3f0af2b6e7bfcbed41f25eb26ddbf9dcff
gggllm <515815530@qq.com>
2019-03-08 23:57:47 -0500

    modify router to support unified result processing

--- app.rb
+++ app.rb
@@ -98,8 +98,7 @@ class App < Sinatra::Base
 
 
   get '/*' do
-    #send_file File.join(settings.public_folder, 'index.html')
-    redirect '/index.html'
+    send_file File.join(settings.public_folder, 'index.html')
   end
 
   run! if app_file == $0
