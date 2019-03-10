ca0bd580fcd831bb6cd28cff208697642f3516d4
gggllm <515815530@qq.com>
2019-03-10 19:19:10 -0400

    add a rake task called seed to seed data to mongodb

--- seed/seed.rb
+++ seed/seed.rb
@@ -5,11 +5,11 @@ require_relative '../model/tweet.rb'
 require_relative '../model/followee.rb'
 require_relative '../model/follower.rb'
 # DB Setup
-Mongoid.load! "../config/mongoid.yml", :test
+Mongoid.load! "config/mongoid.yml", :test
 
-follows = File.read('./follows.csv')
-tweets = File.read('./tweets.csv')
-users = File.read('./users.csv')
+follows = File.read('seed/follows.csv')
+tweets = File.read('seed/tweets.csv')
+users = File.read('seed/users.csv')
 
 User.delete_all
 Follower.delete_all
@@ -19,11 +19,19 @@ Tweet.delete_all
 
 user_hash = {}
 users.split(/\n/).map {|x| x.split(',')}.each do |array|
-  user_hash[array[0]] = User.create(name: array[1])
-  puts array[0]
+  user_hash[array[0]] = User.create(name: array[1]).id
+  puts array[1]
 end
 
 
 Tweet.create(tweets.split(/\n/).map {|x| x.split(',')}.map {|array| {content: array[1],user_id:user_hash[array[0]]}})
 
-follow=follows.split(/\n/).map {|x| x.split(',')}.map {|array| {}}
+follows.split(/\n/).map {|x| x.split(',')}.map {|array| {follower:array[0],followee:array[1]}}
+
+followers=follows.group_by{|x|user_hash[x[:followee]]}.map{|key,value|{user_id:key,followers:value}}
+
+followees=follows.group_by{|x|user_hash[x[:follower]]}.map{|key,value|{user_id:key,followees:value}}
+
+Followee.create(followees)
+
+Follower.create(followers)
