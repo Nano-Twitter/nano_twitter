cfbc029d767662edff7592014265dbc815661599
kudari00 <yehong@brandeis.edu>
2019-03-10 15:31:31 -0400

    todos

--- services/tweet_service.rb
+++ services/tweet_service.rb
@@ -11,11 +11,18 @@ class TweetService
     param params: a hashmap containing info of a new tweet
     return: an json response
     """
-    tweet = Tweet.new(params)
-    if tweet.save
-      json_result(201, 0, "Tweet sent successfully.", tweet)
-    else
-      json_result(403, 1, "Unable to send the tweet.", {})
+    if params[:content] == '' && params[:parent_id] != ''  # if it is a repost
+      params[:content] = 'Repost'  # add 'Repost' to the tweet content
+    end
+    if params[:content] == ''  # the tweet has no content
+      json_result(403, 7, "Your tweet should not be empty.", {}) 
+    else  # the tweet has content
+      tweet = Tweet.new(params)
+      if tweet.save
+        json_result(201, 0, "Tweet sent successfully.", tweet)
+      else
+        json_result(403, 1, "Unable to send the tweet.", {})
+      end
     end
   end
 
@@ -36,10 +43,10 @@ class TweetService
   def get_tweet(params)
     """
     Get a tweet
-    param params: 
+    param params: a hash containing the id of a tweet
     """
     tweet = Tweet.find(params[:id])
-    if tweet:
+    if tweet
       json_result(201, 0, "Tweet found.", tweet)
     else
       json_result(400, 1, "Tweet not found", {})
@@ -47,6 +54,21 @@ class TweetService
   end
 
   def get_tweets_by_user(params, start, count)
-    tweet = Tweet.where(user_id: params[:user_id])
+    """
+    Get a list of tweets
+    param params: a hash containing the user_id of the requested tweet
+    """
+    tweets = Tweet.where(user_id: params[:user_id]).order(created_at: :desc).skip(start).limit(count)
+    if tweets
+      json_result(201, 0, "Tweets found.", tweets)
+    else
+      json_result(201, 1, "Tweets not found.", {})
+    end
   end
+
+  def get_total_by_user(params)
+    tweets = Tweet.where(user_id: params[:user_id]).count
+  end
+
+  def get_followee_tweets(params)
 end
