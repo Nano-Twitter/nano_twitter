2eb838036c200accece2be51b9a639d43e79fa23
gggllm <515815530@qq.com>
2019-03-24 21:40:30 -0400

    add redis support,waiting for using.

--- helper/service_helper.rb
+++ helper/service_helper.rb
@@ -1,5 +1,11 @@
 require 'json'
 
+@redis = Redis.new(host: 'nanotwitter.aouf4s.0001.use2.cache.amazonaws.com', port: 6379)
+
+def redis
+  @redis
+end
+
 def json_result(status, code, message, data = {})
   {
       status: status,
