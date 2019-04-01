f1f8bd27a16e663c6822ac9e2cd28c561703023e
chauncy <chauncyf@qq.com>
2019-04-01 00:32:58 -0400

    fix recom route

--- app.rb
+++ app.rb
@@ -42,20 +42,20 @@ class App < Sinatra::Base
     session[:user] != nil ? @user = session[:user] : nil
   end
 
-# Endpoints
+  # Endpoints
 
   get '/' do
     ":)"
   end
-# Users
+  # Users
 
-# sign up： create a new user
+  # sign up： create a new user
   post '/users/signup' do
     @result = UserService.signup(params)
     pass
   end
 
-# find a user's info
+  # find a user's info
   get '/users/:id' do
     if params[:id] == 'c'
       params[:id] = session[:id]
@@ -64,14 +64,15 @@ class App < Sinatra::Base
     pass
   end
 
-# update a user's info
+
+  # update a user's info
   put '/users/:id' do
     @result = UserService.update_profile(params)
     pass
   end
 
-# user authencation
-# sign in
+  # user authencation
+  # sign in
   post '/users/signin' do
     @result = UserService.login(params)
     if @result[:status] == 200
@@ -81,171 +82,170 @@ class App < Sinatra::Base
     pass
   end
 
-# sign out
-# better use post here
+  get '/users_recommend' do
+    @result = UserService.recommend(params)
+  end
+  # sign out
+  # better use post here
   delete '/users/signout' do
     session[:user] = nil;
     @result = UserService.signout
     pass
   end
 
-  get '/users/recommend/:id' do
-    @result = UserService.recommend(params)
-  end
 
+  # Follow
 
-# Follow
-
-# get all followee ids
-# not so useful?
+  # get all followee ids
+  # not so useful?
   get '/followees/ids/:id' do
     if params[:id] == 'c'
       params[:id] = session[:id]
     end
   end
 
-# get all follower ids
+  # get all follower ids
   get '/followers/list/:id' do
     if params[:id] == 'c'
       params[:id] = session[:id]
     end
   end
 
-# get all followees
+  # get all followees
   get '/followees/list/:id' do
     if params[:id] == 'c'
       params[:id] = session[:id]
     end
   end
 
-# add follower
+  # add follower
   post '/follows/:followee_id' do
     params[:follower_id] = session[:id]
     @result = FollowService.follow(params)
     pass
   end
 
-# unfollow
+  # unfollow
   delete '/follows/:followee_id' do
     params[:follower_id] = session[:id]
     @result = FollowService.unfollow(params)
     pass
   end
 
-# Tweets
+  # Tweets
 
-# get a user's timeline
+  # get a user's timeline
   get '/tweets/users/:user_id' do
     @result = TweetService.get_tweets_by_user(params)
     pass
   end
 
-# get personal homepage timeline
+  # get personal homepage timeline
   get '/tweets/recent' do
     # @result = {self: TweetService.get_tweets_by_user(params), other: TweetService.get_followee_tweets(params)}
     @result = TweetService.get_tweets_by_user(params)
     process_result
   end
 
-# create a tweet
+  # create a tweet
   post '/tweets' do
     @result = TweetService.create_tweet(params)
     pass
   end
 
-# retrieve a tweet
+  # retrieve a tweet
   get '/tweeto/:id' do
     @result = TweetService.get_tweet(params)
     pass
   end
 
-# delete a tweet
+  # delete a tweet
   delete '/tweets/:id' do
     @result = TweetService.delete_tweet(params)
     pass
   end
 
-# get a user's timeline
+  # get a user's timeline
   get '/tweets/users/:user_id' do
     @result = TweetService.get_tweets_by_user(params)
     pass
   end
 
-# Tweet: Comment
+  # Tweet: Comment
 
-# count the number of comments
-# not so useful
+  # count the number of comments
+  # not so useful
   get '/tweets/:tweet_id/comments/count' do
     @result = CommentService.total_comment_by_tweet(params)
     pass
   end
 
-# create a comment
+  # create a comment
   post '/tweets/:tweet_id/comments' do
     @result = CommentService.create_comment(params)
     pass
   end
 
-# retrieve comments of a tweet
+  # retrieve comments of a tweet
   get '/tweets/:tweet_id/comments' do
     @result = CommentService.get_comment_by_tweet(params)
     pass
   end
 
-# delete a comment of a tweet
+  # delete a comment of a tweet
   delete '/tweets/:tweet_id/comments/:comment_id' do
     @result = CommentService.delete_comment(params)
     pass
   end
 
-# Tweet: Like
+  # Tweet: Like
 
-# count the number of likes
+  # count the number of likes
   get '/tweets/:tweet_id/likes/count' do
     @result = LikeService.total_likes_by_tweet(params)
     pass
   end
 
-# create a like of a tweet (like)
+  # create a like of a tweet (like)
   post '/tweets/:tweet_id/likes' do
     @result = LikeService.create_like(params)
     pass
   end
 
-# delete a like of a tweet (unlike)
+  # delete a like of a tweet (unlike)
   delete '/tweets/:tweet_id/likes/:like_id' do
     @result = LikeService.delete_like(params)
     pass
   end
 
-# Search (Blank for the moment)
+  # Search (Blank for the moment)
 
 
-# # for protected routes
-#   get '/example_protected_route', :auth => :user do
-#     "I am protected"
-#   end
+  # # for protected routes
+  #   get '/example_protected_route', :auth => :user do
+  #     "I am protected"
+  #   end
 
 
-# test interface
+  # test interface
 
 
-# If needed deletes all users, tweets, follows
-# Recreates TestUser
-# Example: test/reset/all
+  # If needed deletes all users, tweets, follows
+  # Recreates TestUser
+  # Example: test/reset/all
   post '/test/reset/all' do
     @result = TestService.reset
     pass
   end
 
 
-# Deletes all users, tweets and follows
-# Recreate TestUser
-# Imports data from standard seed data, see: Seed Data
-#   ?users=n means to import n users from the seed data…
-#   Including all the related follows (i.e. both users need to be present)
-#   And all the related tweets
-# Example: `/test/reset/standard?users=100&tweets=100
+  # Deletes all users, tweets and follows
+  # Recreate TestUser
+  # Imports data from standard seed data, see: Seed Data
+  #   ?users=n means to import n users from the seed data…
+  #   Including all the related follows (i.e. both users need to be present)
+  #   And all the related tweets
+  # Example: `/test/reset/standard?users=100&tweets=100
   post '/test/reset' do
     number = params[:users]
     @result = TestService.seed_user_and_related(number)
@@ -253,8 +253,8 @@ class App < Sinatra::Base
   end
 
 
-# {u} can be the user id of some user, or the keyword testuser
-# n is how many randomly generated tweets are submitted on that users behalf
+  # {u} can be the user id of some user, or the keyword testuser
+  # n is how many randomly generated tweets are submitted on that users behalf
   post '/test/user/:id/tweets' do
     count = params[:count]
     user_id = params[:id]
@@ -263,17 +263,17 @@ class App < Sinatra::Base
   end
 
 
-# One page “report”:
-#   How many users, follows, and tweets are there
-#   What is the TestUser’s id
-# Example: /test/status
+  # One page “report”:
+  #   How many users, follows, and tweets are there
+  #   What is the TestUser’s id
+  # Example: /test/status
   get '/test/status' do
     @result = TestService.status
     pass
   end
 
 
-# following are route end point, will only be accessed when calling pass in the previous route
+  # following are route end point, will only be accessed when calling pass in the previous route
 
 
   get '/*' do
