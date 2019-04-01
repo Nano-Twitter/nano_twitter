4a4fbe105ed48aa0331c714ba7179f3fb34c8460
kudari00 <yehong@brandeis.edu>
2019-04-01 09:59:28 -0400

    ...

--- public/precache-manifest.c550a8886cebc540383e70b93dd22db9.js
+++ public/precache-manifest.c550a8886cebc540383e70b93dd22db9.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "483fc4ef5592b64e2050",
-    "url": "/static/js/main.483fc4ef.chunk.js"
+    "revision": "0b0b16e4fb3a315f1c9a",
+    "url": "/static/js/main.0b0b16e4.chunk.js"
   },
   {
     "revision": "2420aac057ec61a6eac1",
     "url": "/static/js/2.2420aac0.chunk.js"
   },
   {
-    "revision": "82c2c670bfb30e6ce469ffa9b872fcee",
+    "revision": "03625c72cf96f4733937254ab9a43c7e",
     "url": "/index.html"
   }
 ];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a6151b95563f6e34d15a6165b33a184.js"
+  "/precache-manifest.c550a8886cebc540383e70b93dd22db9.js"
 );
 
 workbox.clientsClaim();
