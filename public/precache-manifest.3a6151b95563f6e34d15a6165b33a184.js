99cf916e77a3aeeb31841fc341d11639fc1f92e1
kudari00 <yehong@brandeis.edu>
2019-04-01 09:57:32 -0400

    ...

---
+++ public/precache-manifest.3a6151b95563f6e34d15a6165b33a184.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "483fc4ef5592b64e2050",
+    "url": "/static/js/main.483fc4ef.chunk.js"
+  },
+  {
+    "revision": "2420aac057ec61a6eac1",
+    "url": "/static/js/2.2420aac0.chunk.js"
+  },
+  {
+    "revision": "82c2c670bfb30e6ce469ffa9b872fcee",
+    "url": "/index.html"
+  }
+];
