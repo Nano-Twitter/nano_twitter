afefa1b176658d714059a0bc16bd96e74d53d38f
chauncy <chauncyf@qq.com>
2019-04-17 15:53:27 -0400

    config redis

--- helper/redis_helper.rb
+++ helper/redis_helper.rb
@@ -4,43 +4,47 @@ require 'connection_pool'
 
 # $redis = Redis.new(host: 'nanotwitter.aouf4s.0001.use2.cache.amazonaws.com', port: 6379)
 # $redisStore = Redis.new(host: 'localhost', port: 6379)
-$redisStore = ConnectionPool::Wrapper.new(size: 5, timeout: 3) { Redis.new(host: 'localhost', port: 6379) }
-
+#
+begin
+  $redisStore = ConnectionPool::Wrapper.new(size: 5, timeout: 3) {Redis.new(host: ENV['REDIS_URL'])}
+rescure
+  $redisStore = ConnectionPool::Wrapper.new(size: 5, timeout: 3) {Redis.new(host: 'localhost', port: 6379)}
+end
 
 def cached?(store, key)
-    store.exists(key)
+  store.exists(key)
 end
 
 # timeline
 # user_id: [tweet_id1, tweet_id2, ...]
 def push_mass_tweets(store, key, tweets)
-    tweets.each do |t|
-        store.lpush(key, t)
-        store.expire(key, 24.hours.to_i)
-    end
+  tweets.each do |t|
+    store.lpush(key, t)
+    store.expire(key, 24.hours.to_i)
+  end
 end
 
 def get_timeline(store, key, start, count)
-    store.lrange(key, start, start + count - 1)
+  store.lrange(key, start, start + count - 1)
 end
 
 def push_single_tweet(store, key, tweet)
-    store.lpush(key, tweet)
-    store.expire(key, 120.hours.to_i)
+  store.lpush(key, tweet)
+  store.expire(key, 120.hours.to_i)
 end
 
 # user info cache
 # user: user_info_json
 def push_single_user(store, key, user)
-    store.set(key, user.to_json)
-    store.expire(key, 24.hours.to_i)
-end 
+  store.set(key, user.to_json)
+  store.expire(key, 24.hours.to_i)
+end
 
 def get_single_user(store, key)
-    # JSON.parse(JSON.parse(store.get(key)))
-    JSON.parse(store.get(key))
+  # JSON.parse(JSON.parse(store.get(key)))
+  JSON.parse(store.get(key))
 end
 
 def clear(store)
-    store.flushall
+  store.flushall
 end
