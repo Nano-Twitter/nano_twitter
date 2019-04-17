6e0cfb8a600b4b92e1fb0b7d8cb7ea2545f7af00
gggllm <515815530@qq.com>
2019-04-16 20:55:03 -0400

    fix a lot of bug of mongoid

--- services/test_service.rb
+++ services/test_service.rb
@@ -20,7 +20,7 @@ class TestService
   def self.seed_user_and_related(number)
     Seed.reset
     Seed.create_user_and_related number.to_i
-    json_result(200, 0, "good", {})
+    json_result(200, 0, "#{number.to_i}users created", {})
   end
 
   def self.seed_tweet(user_id, number)
