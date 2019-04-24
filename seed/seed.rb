53ff386fe8822408db7bc28a123b29c23b4b85d6
gggllm <515815530@qq.com>
2019-04-24 00:27:52 -0400

    change destroy_all to bulk delete

--- seed/seed.rb
+++ seed/seed.rb
@@ -11,8 +11,8 @@ class Seed
   @tweets = File.read('./seed/tweets.csv')
 
   def self.reset
-    Tweet.collection.delete_many(Tweet.all)
-    User.collection.delete_many(User.all)
+    Tweet.delete_all
+    User.delete_all
   end
 
   def self.create_user(sum = 100)
