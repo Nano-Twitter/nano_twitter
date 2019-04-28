be50e8988d5065e55fd522cace38abd223501a43
kudari00 <yehong@brandeis.edu>
2019-04-28 13:14:40 -0400

    ufo setting

--- .ufo/variables/production.rb
+++ .ufo/variables/production.rb
@@ -4,4 +4,6 @@
 @environment = helper.env_vars(%Q[
   RAILS_ENV=production
   SECRET_KEY_BASE=secret
+  CLOUDAMQP_URL=amqp://user:wGLzrAvw8nvM@ec2-34-201-19-121.compute-1.amazonaws.com:5672
+  REDIS_URL=redis://ntredis.aouf4s.ng.0001.use2.cache.amazonaws.com:6379
 ])
