c5ba940d834728e605f0fcc0a665697988035b33
gggllm <515815530@qq.com>
2019-04-30 14:17:39 -0400

    updated to the newest test file

--- helper/redis_helper.rb
+++ helper/redis_helper.rb
@@ -47,10 +47,10 @@ class RedisHelper
     # x = 'BSON::ObjectId("507c7f79bcf86cd7994f6c0e")'[16...-2]
     # a = user['id'].to_s
     if user["follower_ids"]
-      user["follower_ids"] = user["follower_ids"].map {|id| id.to_s[0...24]}
+      user["follower_ids"] = user["follower_ids"].map {|id| id.to_s[8...-2]}
     end
     if user["following_ids"]
-      user["following_ids"] = user["following_ids"].map {|id| id.to_s[0...24]}
+      user["following_ids"] = user["following_ids"].map {|id| id.to_s[8...-2]}
     end
 
     @store.hmset("user_#{user_id}", user.as_json.to_a.flatten(1))
